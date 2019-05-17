f8ff54581d0fa524409413020ee5d044d4281561
Caian R. Ertl <hi@caian.org>
2019-05-16 21:49:15 -0300

    fix(zsh): change PATH directories

--- dots/zsh/._path.sh
+++ dots/zsh/._path.sh
@@ -1,14 +1,11 @@
 # muh binaries
-USER_BIN_DIR=$HOME/bin:$HOME/.local/bin
+USER_BIN="$HOME/bin:$HOME/.local/bin"
 
 # golang compiler
-GO_PATH=$HOME/bin/golang/bin
-
-# rust compiler & cargo
-RUST_PATH=$HOME/.cargo/bin
+GO_PATH="$HOME/bin/golang/bin"
 
 # ruby version manager
-RVM_PATH=$HOME/.rvm/bin
+RVM_PATH="$HOME/.rvm/bin"
 
 # export the whole shebang
-export PATH=$PATH:$USER_BIN_DIR:$GO_PATH:$RUST_PATH:RVM_PATH
+export PATH="$PATH:$USER_BIN:$GO_PATH:$RVM_PATH"
