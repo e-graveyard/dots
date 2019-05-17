90112b76962cd8d5ef1c7f116e3a156424c2786a
Caian R. Ertl <hi@caian.org>
2019-05-17 03:29:15 -0300

    zsh: comply to SC2139, aliases for ls_extended

--- pkgs/zsh/._alias.sh
+++ pkgs/zsh/._alias.sh
@@ -3,27 +3,29 @@ alias b="bat"
 alias c="clear"
 alias e="emacs -nw"
 alias v="nvim"
-alias t="tmux -f $HOME/.config/tmux/.tmux.conf"
+alias t='tmux -f $HOME/.config/tmux/.tmux.conf'
+alias l="ls_extended -lhA"
+alias ll="ls_extended -lh"
 
 # configuration files
-alias _vim="v $VIM_PROF/.vimrc $VIM_PROF/.vim/*.vim"
-alias _zsh="v $DOTS_DIR/zsh/.zshrc"
-alias _tmux="v $DOTS_DIR/tmux/.config/tmux/.tmux.conf"
-alias _emacs="v $DOTS_DIR/emacs/.emacs"
+alias _vim='v $VIM_PROF/.vimrc $VIM_PROF/.vim/*.vim'
+alias _zsh='v $DOTS_DIR/zsh/.zshrc'
+alias _tmux='v $DOTS_DIR/tmux/.config/tmux/.tmux.conf'
+alias _emacs='v $DOTS_DIR/emacs/.emacs'
 
 # shell externals
-alias _env="v $DOTS_DIR/shext/.sh_env"
-alias _path="v $DOTS_DIR/shext/.sh_path"
-alias _alias="v $DOTS_DIR/shext/.sh_alias"
+alias _env='v $DOTS_DIR/shext/.sh_env'
+alias _path='v $DOTS_DIR/shext/.sh_path'
+alias _alias='v $DOTS_DIR/shext/.sh_alias'
 
 # common directories
-alias gmed="cd /run/media/$USER"
-alias gdoc="cd $HOME/Documents"
-alias gdow="cd $HOME/Downloads"
-alias gpic="cd $HOME/Pictures"
-alias gvid="cd $HOME/Videos"
-alias gmus="cd $HOME/Music"
-alias gbin="cd $HOME/bin"
+alias gmed='cd /run/media/$USER'
+alias gdoc='cd $HOME/Documents'
+alias gdow='cd $HOME/Downloads'
+alias gpic='cd $HOME/Pictures'
+alias gvid='cd $HOME/Videos'
+alias gmus='cd $HOME/Music'
+alias gbin='cd $HOME/bin'
 
 # common actions
-alias uzc="source $HOME/.zshrc"
+alias uzc='source $HOME/.zshrc'
