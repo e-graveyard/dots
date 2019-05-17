3d0ef5f1429829b68f9bc2465bc88611d0bdb4bd
Caian R. Ertl <hi@caian.org>
2019-05-16 21:55:50 -0300

    fix(zsh): change directory locations

--- dots/zsh/._env.sh
+++ dots/zsh/._env.sh
@@ -1,4 +1,4 @@
 # git & dots-related
-export PROJ_DIR=$HOME/Documents/GitHub
-export INIT_DIR=$PROJ_DIR/init/profiles
-export VIM_PROF=$INIT_DIR/vim
+export PROJ_DIR="/run/media/cai/warehouse/git"
+export DOTS_DIR="$HOME/dots/pkgs"
+export VIM_PROF="$DOTS_DIR/vim"
