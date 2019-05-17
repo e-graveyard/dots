cd75b786ea06ae49ce7da93f2932cf4afac3643f
Caian R. Ertl <hi@caian.org>
2019-05-16 22:01:35 -0300

    fix(zsh): use "$USER" env instead of hard-coded username

--- dots/zsh/._env.sh
+++ dots/zsh/._env.sh
@@ -1,4 +1,4 @@
 # git & dots-related
-export PROJ_DIR="/run/media/cai/warehouse/git"
+export PROJ_DIR="/run/media/$USER/warehouse/git"
 export DOTS_DIR="$HOME/dots/pkgs"
 export VIM_PROF="$DOTS_DIR/vim"
