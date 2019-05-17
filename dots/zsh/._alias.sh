fd783dd20995f6d8bc941546673ed1e7ed4c2415
Caian R. Ertl <hi@caian.org>
2019-05-16 22:10:24 -0300

    fix(zsh): change aliases

--- dots/zsh/._alias.sh
+++ dots/zsh/._alias.sh
@@ -1,46 +1,29 @@
 # programs
+alias b="bat"
 alias c="clear"
 alias e="emacs -nw"
 alias v="nvim"
-alias tx="fetch_tmux_plugins && tmux -f ~/.config/tmux/.tmux.conf"
-alias kpc="perl $PROJ_DIR/kpclix/kpcli.pl --histfile /dev/null --kdb /media/cai/PD/vault/keychain.kdbx --timeout=600 --no-recycle"
-alias hups="hangups --config ~/.config/hangups/hangups.conf"
-
-# vim bundle
-alias vvim="v $VIM_PROF/.vimrc $VIM_PROF/.vim/*.vim"
+alias t="tmux -f $HOME/.config/tmux/.tmux.conf"
 
 # configuration files
-alias vi3="v $INIT_DIR/i3/bin/source/rwal/templates/i3"
-alias vzsh="v $INIT_DIR/zsh/.zshrc"
-alias vbash="v $INIT_DIR/bash/.bashrc"
-alias vtmux="v $INIT_DIR/tmux/.config/tmux/.tmux.conf"
-alias vhups="v $INIT_DIR/hangups/.config/hangups/hangups.conf"
-alias vemacs="v $INIT_DIR/emacs/.emacs"
+alias _vim="v $VIM_PROF/.vimrc $VIM_PROF/.vim/*.vim"
+alias _zsh="v $DOTS_DIR/zsh/.zshrc"
+alias _tmux="v $DOTS_DIR/tmux/.config/tmux/.tmux.conf"
+alias _emacs="v $DOTS_DIR/emacs/.emacs"
 
 # shell externals
-alias venv="v $INIT_DIR/shext/.sh_env"
-alias vpath="v $INIT_DIR/shext/.sh_path"
-alias valias="v $INIT_DIR/shext/.sh_alias"
+alias _env="v $DOTS_DIR/shext/.sh_env"
+alias _path="v $DOTS_DIR/shext/.sh_path"
+alias _alias="v $DOTS_DIR/shext/.sh_alias"
 
 # common directories
-alias gmed="cd /media/$USER"
-alias gdoc="cd ~/Documents"
-alias gorg="cd ~/Documents/Org"
-alias ggit="cd ~/Documents/GitHub"
-alias gdow="cd ~/Downloads"
-alias gimg="cd ~/Images"
-alias gbin="cd ~/bin"
-alias gdmp="cd ~/Dump"
-alias gmus="cd ~/Music"
-
-# repositories
-alias gdora="cd $PROJ_DIR/dora"
-alias ginit="cd $PROJ_DIR/init"
-alias gcoin="cd $PROJ_DIR/coin"
-alias gsani="cd $PROJ_DIR/sanity"
-alias gkpcx="cd $PROJ_DIR/kpclix"
-alias gorgv="cd $PROJ_DIR/org.vim"
-alias grstv="cd $PROJ_DIR/rst.vim"
+alias gmed="cd /run/media/$USER"
+alias gdoc="cd $HOME/Documents"
+alias gdow="cd $HOME/Downloads"
+alias gpic="cd $HOME/Pictures"
+alias gvid="cd $HOME/Videos"
+alias gmus="cd $HOME/Music"
+alias gbin="cd $HOME/bin"
 
 # common actions
-alias uzc="source ~/.zshrc"
+alias uzc="source $HOME/.zshrc"
