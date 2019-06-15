61831ca5bc08028c260e48ce6e9fc85003f96405
Caian R. Ertl <hi@caian.org>
2019-06-15 01:41:40 -0300

    zsh: make the custom functions externally accesible

---
+++ pkgs/zsh/._func.sh
@@ -0,0 +1,48 @@
+# create a dir (with parents) and cd into them
+m() {
+    mkdir -p -- "$1" &&
+        cd -P -- "$1" || return
+}
+
+# concatenate my Xresources and pywal's generated colorscheme and update
+# the database
+reload_xrdb() {
+    cat "$HOME/.Xresources" "$HOME/.cache/wal/colors.Xresources" | xrdb -
+}
+
+# download a mp3 from youtube (w/ the best quality possible)
+ytd() {
+    youtube-dl \
+        --extract-audio \
+        --audio-format mp3 \
+        --audio-quality 0 \
+        "https://www.youtube.com/watch?v=$1"
+}
+
+# change wallpaper for all workspaces
+chwp() {
+    dbus-send \
+        --session \
+        --dest=org.kde.plasmashell \
+        --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript "string:
+        var Desktops = desktops();
+        for (i=0;i<Desktops.length;i++) {
+                d = Desktops[i];
+                d.wallpaperPlugin = 'org.kde.image';
+                d.currentConfigGroup = Array('Wallpaper', 'org.kde.image', 'General');
+                d.writeConfig('Image', 'file://$1');
+        }"
+}
+
+# set the theme from an image
+chtm() {
+    wal -n -i "$@"
+    chwp "$(cat "$HOME/.cache/wal/wal")"
+    reload_xrdb
+}
+
+# reload the xresources database before opening emacs
+org_mode() {
+    reload_xrdb
+    nohup emacs --fullscreen & disown
+}
